172279ccb851f54ee5a0f0321af723c547096119
Eduard Bruhosh <edyabrug91@gmail.com>
2023-02-11 20:27:57 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// створення розмітки
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join('');
+}
+
+// Відображення розмітки в браузері
+const imageCardsContainer = document.querySelector('.gallery');
+imageCardsContainer.insertAdjacentHTML('beforeend', createImageCardsMarkup(galleryItems));
+
+// логіка модального вікна
+imageCardsContainer.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  // скидання поведінки браузера за замовчуванням
+  event.preventDefault();
+
+  // перевірка чи клік відбувається по таргету
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  // Створення базової розмітки та відкриття модалки
+  const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`);
+  instance.show();
+
+  // Заміна значення атрибута src елемента <img> в модальному вікні
+  const lightboxImage = document.querySelector('[src="assets/images/image.png"]');
+  lightboxImage.src = event.target.dataset.source;
+
+  // Закриття модалки по кнопці escape
+  window.addEventListener('keydown', onEscapePress);
+
+  function onEscapePress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+const imageCardsContainer = document.querySelector('.gallery');
+imageCardsContainer.insertAdjacentHTML('beforeend', createImageCardsMarkup(galleryItems));
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
